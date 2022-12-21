0a9ee84add86f780ee9b1fcb0614ba3d2c149af7
Egor Ermolaev <egor.ermolaev@uni.lu>
2022-12-21 13:05:42 +0100

    Implement before/beforeEach preparation for testing

--- heimdalljs/src/revocation.spec.js
+++ heimdalljs/src/revocation.spec.js
@@ -1,14 +1,35 @@
 const expect = require('chai').expect;
+const fs = require('fs').promises;
 
-const sum = (array) => array.reduce((a, b) => a + b, 0)
+const {RevocationRegistry} = require('./revocation');
+const {merklePoseidon} = require('../src/crypto/poseidon.js');
+const {signPoseidon} = require('../circomlib/eddsa');
 
-describe('RevocationRegistry class', function () {
-    beforeEach(function () {
-        //TODO: instantiate a new one before each
-    })
+const secretKeyPath = './test/attribute/ca_sk.txt';
+const sum = (array) => array.reduce((a, b) => a + b, 0);
 
-    it('[METHOD] update()', function () {
-        // add an assertion
-        expect(sum([1, 2, 3, 4, 5])).to.equal(15);
-    })
-})
+describe('RevocationRegistry class', () => {
+    let secretKey;
+    before(async () => {
+        console.debug('Debug -> before: Loading the input files');
+        secretKey = await fs.readFile(secretKeyPath, 'utf8');
+        console.debug('Debug -> before: secretKey >> ', secretKey);
+    });
+
+    after(async () => {
+        console.debug('Debug -> after: cleaning after testing');
+    });
+
+    let revocationRegistry;
+    beforeEach(() => {
+        console.debug('Debug -> beforeEach: (re)instantiation RevocationRegistry');
+        revocationRegistry = new RevocationRegistry(secretKey, merklePoseidon, (sk, msg) => signPoseidon(sk, BigInt(msg)));
+        console.debug('Debug -> beforeEach: revocationRegistry >> ', revocationRegistry);
+    });
+
+    describe('Performing the tests', () => {
+        it('[METHOD] update()', () => {
+            expect(sum([1, 2, 3, 4, 5])).to.equal(15);
+        });
+    });
+});
