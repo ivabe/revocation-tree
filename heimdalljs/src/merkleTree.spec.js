405b787482d19ae8c303c26f97b814c60febb68d
Iván A <ivan.abellan@uni.lu>
2023-01-04 13:29:27 +0100

    test merkle tree proof with credential process.mainModule deprecated instead require.main

--- heimdalljs/src/merkleTree.spec.js
+++ heimdalljs/src/merkleTree.spec.js
@@ -1,9 +1,19 @@
 const expect = require('chai').expect;
 const assert = require('chai').assert;
 const fs = require('fs').promises;
+const path = require('path');
 
 const {MerkleTree} = require('./crypto/merkleTree');
 const {merklePoseidon} = require('./crypto/poseidon.js');
+const {signPoseidon} = require("../circomlib/eddsa.js");
+const {getSecretKey, getRevocationTree} = require("../cli/util");
+
+//Verifiable credential components
+const {AttributePresentation} = require("./presentation/attribute");
+const credential = require('../test/attribute/cred_holder.json');
+const secret_key_holder_path = './test/attribute/holder_sk.txt';
+const public_key_issuer = require('../test/attribute/issuer_pk.json');
+const { exit } = require('process');
 
 const generateZeroArray = (numberOfLeaves, mapOfPositions = []) => {
     const zeroArray = new Array(numberOfLeaves).fill(0);
@@ -30,6 +40,45 @@ const testMerkleProofBit = (MerkleTree, position) => {
     expect(proof.validate()).to.equal(false);
 };
 
+const testMerkleProofOnCredential = async (credential, position, cred_type) => {
+    if(cred_type == 'attribute'){
+        try {
+            //console.debug(credential);
+            let undefined; // force an 'undefined' variable
+            // the code forces a credential to have a revocation tree
+            // get the revocation tree from the credential
+            let revocationTree = await getRevocationTree(treeName = undefined, source = credential.attributes[4]);
+            //read holder secret key 
+            let secretKey = await getSecretKey(secret_key_holder_path);
+            let presentation = new AttributePresentation(
+                credential          = credential,
+                expiration          = credential.attributes[5],
+                revocation          = revocationTree,
+                challenge           = 1234,
+                sk                  = secretKey,
+                issuerPk            = public_key_issuer,
+                signatureGenerator  = signPoseidon,
+                treeGenerator       = merklePoseidon,
+                index               = Number(position)
+            );
+            //console.debug(presentation);
+            // generate merkle proof
+            let proof = await presentation.generate();
+            expect(proof).to.equal(true); // implicitly runs verifyProof too
+            //console.debug(presentation.output);
+            // check output value was confined into the credential
+            expect(credential.attributes).that.does.include(presentation.output['content']['attribute']);
+            return Promise.resolve(true); 
+        } catch (error) {
+            return Promise.reject(false);
+        }
+        
+    }else{
+        console.log("Credential type: " + cred_type + " not supported\n" );
+        return Promise.reject(false);
+    }
+};
+
 describe('MerkleTree class', () => {
     let merkleTree;
     before(async () => {
@@ -39,6 +88,7 @@ describe('MerkleTree class', () => {
 
     after(async () => {
         console.debug('Debug -> after: cleaning after testing');
+        //process.exit(0);
     });
 
     describe('Tree generation', () => {
@@ -56,6 +106,7 @@ describe('MerkleTree class', () => {
         it('generateProof(0)', async () => {
             testMerkleProofBit(merkleTree, 0);
         });
+        /*
         it('generateProof(1)', async () => {
             testMerkleProofBit(merkleTree, 1);
         });
@@ -74,6 +125,15 @@ describe('MerkleTree class', () => {
             testMerkleProofBit(merkleTree, merkleTree.leaves.length+1);
         });
         */
+        it('generateProof_from_credential()', async () => {
+            let res = await testMerkleProofOnCredential(credential, 8, 'attribute');
+     
+            // attribute[8] = 'John'
+            //console.log(res);
+            expect(res).to.be.equal(true);
+            //console.log("hrllo end\n");
+        });
+
     });
 
 });
